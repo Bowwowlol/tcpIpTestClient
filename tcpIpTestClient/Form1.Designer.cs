30c64130b31a4f88466ad778960cab15bcf65e15
Bowwowlol <a603060@gmail.com>
2018-07-13 23:14:06 +0800

    加入專案檔案。

---
+++ tcpIpTestClient/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace tcpIpTestClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_IPaddress = new System.Windows.Forms.TextBox();
+            this.textBoxPort = new System.Windows.Forms.TextBox();
+            this.Connect_button = new System.Windows.Forms.Button();
+            this.Close_Button = new System.Windows.Forms.Button();
+            this.listBox_Info = new System.Windows.Forms.ListBox();
+            this.Send_button = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ip Address";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Port";
+            // 
+            // textBox_IPaddress
+            // 
+            this.textBox_IPaddress.Location = new System.Drawing.Point(74, 12);
+            this.textBox_IPaddress.Name = "textBox_IPaddress";
+            this.textBox_IPaddress.Size = new System.Drawing.Size(205, 22);
+            this.textBox_IPaddress.TabIndex = 2;
+            this.textBox_IPaddress.TextChanged += new System.EventHandler(this.textBox_IPaddress_TextChanged);
+            // 
+            // textBoxPort
+            // 
+            this.textBoxPort.Location = new System.Drawing.Point(74, 58);
+            this.textBoxPort.Name = "textBoxPort";
+            this.textBoxPort.Size = new System.Drawing.Size(205, 22);
+            this.textBoxPort.TabIndex = 3;
+            this.textBoxPort.TextChanged += new System.EventHandler(this.textBoxPort_TextChanged);
+            // 
+            // Connect_button
+            // 
+            this.Connect_button.Location = new System.Drawing.Point(12, 127);
+            this.Connect_button.Name = "Connect_button";
+            this.Connect_button.Size = new System.Drawing.Size(75, 23);
+            this.Connect_button.TabIndex = 4;
+            this.Connect_button.Text = "Connect";
+            this.Connect_button.UseVisualStyleBackColor = true;
+            this.Connect_button.Click += new System.EventHandler(this.Connect_Click);
+            // 
+            // Close_Button
+            // 
+            this.Close_Button.Location = new System.Drawing.Point(204, 127);
+            this.Close_Button.Name = "Close_Button";
+            this.Close_Button.Size = new System.Drawing.Size(75, 23);
+            this.Close_Button.TabIndex = 5;
+            this.Close_Button.Text = "Close";
+            this.Close_Button.UseVisualStyleBackColor = true;
+            this.Close_Button.Click += new System.EventHandler(this.Close_Click);
+            // 
+            // listBox_Info
+            // 
+            this.listBox_Info.FormattingEnabled = true;
+            this.listBox_Info.ItemHeight = 12;
+            this.listBox_Info.Location = new System.Drawing.Point(12, 168);
+            this.listBox_Info.Name = "listBox_Info";
+            this.listBox_Info.Size = new System.Drawing.Size(267, 172);
+            this.listBox_Info.TabIndex = 6;
+            // 
+            // Send_button
+            // 
+            this.Send_button.Location = new System.Drawing.Point(110, 127);
+            this.Send_button.Margin = new System.Windows.Forms.Padding(1);
+            this.Send_button.Name = "Send_button";
+            this.Send_button.Size = new System.Drawing.Size(75, 23);
+            this.Send_button.TabIndex = 7;
+            this.Send_button.Text = "Send";
+            this.Send_button.UseVisualStyleBackColor = true;
+            this.Send_button.Click += new System.EventHandler(this.Send_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 360);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(640, 480);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(664, 852);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Send_button);
+            this.Controls.Add(this.listBox_Info);
+            this.Controls.Add(this.Close_Button);
+            this.Controls.Add(this.Connect_button);
+            this.Controls.Add(this.textBoxPort);
+            this.Controls.Add(this.textBox_IPaddress);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Client";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_Closing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_IPaddress;
+        private System.Windows.Forms.TextBox textBoxPort;
+        private System.Windows.Forms.Button Connect_button;
+        private System.Windows.Forms.Button Close_Button;
+        private System.Windows.Forms.ListBox listBox_Info;
+        private System.Windows.Forms.Button Send_button;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ tcpIpTestClient/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace tcpIpTestClient
+{
+    public partial class Form1 : Form
+    {
+        Socket SckSPort; // 先行宣告Socket
+
+        string RmIp = "127.0.0.1";  // 其中 xxx.xxx.xxx.xxx 為Server端的IP
+        Thread SckSReceiveTd = null;
+        int SPort = 888;
+        bool connectContinue = true;
+        int RDataLen = 640*480;  // 此文Server端和Client端都是用固定長度5在傳送資料~ 可以針對自己的需要改長度 
+        Form2 fm2 = new Form2();
+        public Form1()
+        {
+            InitializeComponent();
+            textBox_IPaddress.Text = RmIp;
+            textBoxPort.Text = SPort.ToString();
+        }
+
+        // 連線
+        private void ConnectServer()
+
+        {
+
+            try
+
+            {
+
+                SckSPort = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                SckSPort.Connect(new IPEndPoint(IPAddress.Parse(RmIp), SPort));
+
+                // RmIp和SPort分別為string和int型態, 前者為Server端的IP, 後者為Server端的Port
+
+
+
+                // 同 Server 端一樣要另外開一個執行緒用來等待接收來自 Server 端傳來的資料, 與Server概念同
+
+                SckSReceiveTd = new Thread(SckSReceiveProc);
+
+                SckSReceiveTd.Start();
+                if(SckSPort.Connected)
+                {
+                    listBox_Info.Items.Add("Connected!!");
+                    listBox_Info.Items.Add("The server IP address is " + RmIp + " and port is " + SPort);
+
+                }
+                //SckSReceiveTd.IsBackground = true;
+            }
+            catch { }
+
+        }
+
+        // SckSReceiveProc 是接受來自 Server 端的資料其函式內容幾乎同 Server 端的 SckSAcceptProc 接收資料的Code~  ,  只差在 Client 只有一個Socket. 
+
+
+
+        private void SckSReceiveProc()
+        {
+
+            try
+            {
+
+                long IntAcceptData;
+
+                byte[] clientData = new byte[RDataLen];
+
+                while (connectContinue)
+                {
+
+                    // 程式會被 hand 在此, 等待接收來自 Server 端傳來的資料
+
+                    IntAcceptData = SckSPort.Receive(clientData);
+
+                    // 往下就自己寫接收到來自Server端的資料後要做什麼事唄~^^”
+
+                    //string S = Encoding.Default.GetString(clientData);
+                    try
+                    {
+                        System.IO.MemoryStream ms = new System.IO.MemoryStream(clientData);
+
+                        ms.Write(clientData, 0, clientData.Length);
+
+                        Bitmap b = (Bitmap)Bitmap.FromStream(ms);
+                       // Bitmap b = new Bitmap((Image)new Bitmap(ms));
+                        //PictureBox px = fm2.GetForm2PictureBox();
+                        //px.Image = b;
+                        //fm2.ShowDialog();
+                        pictureBox1.Image = b;
+                        ms.Dispose();
+                        //SckSSend();
+                        ms.Close();
+                        //Thread.Sleep(1);
+                        //Console.WriteLine(S);
+                    }
+                    catch (ArgumentException aee)
+                    {
+                        
+                    }
+                 
+                    
+
+                }
+
+            }
+
+            catch
+            {
+
+
+
+            }
+        }
+
+
+        // 當然 Client 端也可以傳送資料給Server端~ 和 Server 端的SckSSend一樣, 只差在Client端只有一個Socket
+
+        private void SckSSend()
+
+        {
+
+
+
+            try
+
+            {
+
+                string SendS = "Get image!";
+
+                SckSPort.Send(Encoding.ASCII.GetBytes(SendS));
+
+            }
+
+            catch
+
+            {
+
+
+
+            }
+
+
+
+
+
+        }
+        private void Connect_Click(object sender, EventArgs e)
+        {
+            ConnectServer();
+        }
+
+        private void Close_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                    if (SckSPort.Connected == true)
+                    {
+                        connectContinue = false;
+                        //SckSPort.Shutdown(SocketShutdown.Both);
+                        SckSPort.Close();
+                        SckSReceiveTd.Abort();
+                        listBox_Info.Items.Add("disconnect!\n");
+                    }
+                
+            }
+            catch (Exception ee)
+            {
+                MessageBox.Show(ee.Message);
+            }
+        }
+
+        private void Send_Click(object sender, EventArgs e)
+        {
+            SckSSend();
+        }
+
+        private void textBox_IPaddress_TextChanged(object sender, EventArgs e)
+        {
+            RmIp = textBox_IPaddress.Text;
+            
+        }
+
+        private void textBoxPort_TextChanged(object sender, EventArgs e)
+        {
+            SPort = Convert.ToInt32(textBoxPort.Text);
+        }
+
+        private void Form1_Closing(object sender, FormClosingEventArgs e)
+        {
+            if (SckSPort != null && SckSPort.Connected == true)
+            {
+                connectContinue = false;
+                SckSPort.Shutdown(SocketShutdown.Both);
+                SckSPort.Close();
+                SckSReceiveTd.Abort();
+            }
+        }
+    }
+}
